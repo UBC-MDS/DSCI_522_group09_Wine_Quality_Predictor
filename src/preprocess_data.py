3318e39ee1b2fef6cab4dfd1687c18f2a3dd38b3
GloriaWYY <wanying.ye2020@gmail.com>
2021-11-24 16:42:44 -0800

    Golden rule issue, now this outputs four dataframes.

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,9 +1,11 @@
-# author: Paniz Fazlali
-# date: 2021-11-23
+# author: Paniz Fazlali, Wanying Ye
+# date: 2021-11-24
 
-"""This script takes two file paths. It takes in input_path which includes the raw data. 
-Then, it performs data cleaning, transformating and partitioning. Next, it takes in 
-the output_path to store the cleaned data in.
+"""This script takes two file paths. It takes in the input path which includes the raw data and
+the output directory to store the output data in. It performs data cleaning, transformating and 
+partitioning. This script should have 4 outputs: cleaned but unpreprocessed train dataframe, 
+cleaned but and unpreprocessed test dataframe, cleaned and preprocessed train dataframe and 
+cleaned and preprocessed test dataframe.
 
 Usage: preprocess_data.py --input_path_white=<input_path_white> --input_path_red=<input_path_red> --output_dir=<output_dir>
 
@@ -43,35 +45,30 @@ def main(input_path_white, input_path_red, output_dir):
     # split data
     train_df, test_df = train_test_split(data, train_size=0.7, random_state=123)
 
-    ## I am commenting out the codes below. Reasoning: we only return the train_df and test_df as csv from this script,
-    ## and these are what to be inputted in eda script (and machine learning script). We will move the preprocessor
-    ## code to the machine learning script. TEAMMATES: after reviewing, we can remove these comments to make this 
-    ## script cleaner.
+    X_train, y_train = train_df.drop(columns=['quality']), train_df['quality']
+    X_test, y_test = test_df.drop(columns=['quality']), test_df['quality']
+    
+    # transform data
+    numeric_features = list(X_train.select_dtypes(include='number').columns)
+    binary_features = ['type']
+    preprocessor = make_column_transformer(
+        (StandardScaler(), numeric_features),
+        (OneHotEncoder(drop='if_binary'), binary_features)
+    )
+    columns = numeric_features + binary_features
+    X_train_preprocessed = pd.DataFrame(preprocessor.fit_transform(X_train), index=X_train.index, columns=columns)
+    X_test_preprocessed = pd.DataFrame(preprocessor.transform(X_test), index=X_test.index, columns=columns)
+    
+    # gathering X_train and y_train in one dataset
+    train_df_preprocessed = X_train_preprocessed.copy()
+    train_df_preprocessed['quality'] = y_train
 
-    # X_train, y_train = train_df.drop(columns=['quality']), train_df['quality']
-    # X_test, y_test = test_df.drop(columns=['quality']), test_df['quality']
-    
-    # # transform data
-    # numeric_features = list(X_train.select_dtypes(include='number').columns)
-    # binary_features = ['type']
-    # preprocessor = make_column_transformer(
-    #     (StandardScaler(), numeric_features),
-    #     (OneHotEncoder(drop='if_binary'), binary_features)
-    # )
-    # columns = numeric_features + binary_features
-    # X_train = pd.DataFrame(preprocessor.fit_transform(X_train), index=X_train.index, columns=columns)
-    # X_test = pd.DataFrame(preprocessor.transform(X_test), index=X_test.index, columns=columns)
-    
-    # # gathering X_train and y_train in one dataset
-    # train_df = X_train.copy()
-    # train_df['quality'] = y_train
-
-    # # gathering X_test and y_test in one dataset
-    # test_df = X_test.copy()
-    # test_df['quality'] = y_test
+    # gathering X_test and y_test in one dataset
+    test_df_preprocessed = X_test_preprocessed.copy()
+    test_df_preprocessed['quality'] = y_test
     
     
-    # export data
+    # export cleaned unpreprocessed data
     try:
         train_df.to_csv(f"{output_dir}/train_df.csv")
     except:
@@ -82,6 +79,18 @@ def main(input_path_white, input_path_red, output_dir):
     except:
         os.makedirs(os.path.dirname(f"{output_dir}/test_df.csv"))
         test_df.to_csv(f"{output_dir}/test_df.csv")
+
+    # export cleaned preprocessed data
+    try:
+        train_df_preprocessed.to_csv(f"{output_dir}/train_df_preprocessed.csv")
+    except:
+        os.makedirs(os.path.dirname(f"{output_dir}"))
+        train_df_preprocessed.to_csv(f"{output_dir}/train_df_preprocessed.csv")
+    try:
+        test_df_preprocessed.to_csv(f"{output_dir}/test_df_preprocessed.csv")
+    except:
+        os.makedirs(os.path.dirname(f"{output_dir}"))
+        test_df_preprocessed.to_csv(f"{output_dir}/test_df_preprocessed.csv")
 
 if __name__ == "__main__":
     main(opt["--input_path_white"], opt["--input_path_red"], opt["--output_dir"])
