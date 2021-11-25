7ab43c27cb2f8027237666da965daab14d293d05
Gabe Fairbrother <gfairbrother@gmail.com>
2021-11-25 12:36:43 -0800

    Report framework and updated environment

---
+++ reports/wine_quality_predictor_report/_build/jupyter_execute/report_results.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Results & Discussion
+
+# In[ ]:
+
+
+
+
---
+++ reports/wine_quality_predictor_report/_build/jupyter_execute/report_summary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Summary
+
+# This project is an attempt to build a classification model for predicting wine quality. We will evaluate three models for effectiveness in predicting quality scores in red and white wines.  
+# **Analysis and Results Summary Pending**
